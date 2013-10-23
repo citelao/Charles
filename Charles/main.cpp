97b0bafb146d13134ace3f8cc6bfb6c2b0d19789
Ben Stolovitz <ben@stolovitz.com>
2013-10-23 18:46:54 -0500

    Initial Commit

---
+++ Charles/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Charles
+//
+//  Created by Ben Stolovitz on 10/23/13.
+//  Copyright (c) 2013 Ben Stolovitz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
